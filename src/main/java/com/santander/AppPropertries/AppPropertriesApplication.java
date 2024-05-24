010474a35e79d363f6354a3c4626f0ad6131eb7d
rolemberg <vinnie.nsts@gmail.com>
2024-05-24 17:42:28 -0300

    first commit

---
+++ src/main/java/com/santander/AppPropertries/AppPropertriesApplication.java
@@ -0,0 +1,14 @@
+package com.santander.AppPropertries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPropertriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPropertriesApplication.class, args);
+	}
+}
+
+
