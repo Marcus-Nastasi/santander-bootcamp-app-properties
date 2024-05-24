ead8da7f4861d55ffb19e97713b10642b76a7a5c
rolemberg <vinnie.nsts@gmail.com>
2024-05-24 17:58:32 -0300

    creating default values

--- src/main/java/com/santander/AppPropertries/Domain/EmailSystem.java
+++ src/main/java/com/santander/AppPropertries/Domain/EmailSystem.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class EmailSystem {
 
-    @Value("${name}")
+    @Value("${name:No-Reply}")
     private String name;
-    @Value("${email}")
+    @Value("${email:noreply@gmail.com}")
     private String email;
 
     public String getName() {
